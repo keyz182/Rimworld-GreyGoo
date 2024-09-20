15a7f7b6ec5a7b1fdb0dcba46734b914dc07a46f
Kieran David Evans <keyz182@gmail.com>
2024-09-20 15:23:02 +0100

    Cookie cutter initial commit

---
+++ 1.5/Source/Grey_Goo/Grey_GooDefOf.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Grey_Goo;
+
+[DefOf]
+public static class Grey_GooDefOf
+{
+    // Remember to annotate any Defs that require a DLC as needed e.g.
+    // [MayRequireBiotech]
+    // public static GeneDef YourPrefix_YourGeneDefName;
+    
+    static Grey_GooDefOf() => DefOfHelper.EnsureInitializedInCtor(typeof(Grey_GooDefOf));
+}
---
+++ 1.5/Source/Grey_Goo/Grey_GooMod.cs
@@ -0,0 +1,34 @@
+﻿using Verse;
+using UnityEngine;
+using HarmonyLib;
+
+namespace Grey_Goo;
+
+public class Grey_GooMod : Mod
+{
+    public static Settings settings;
+
+    public Grey_GooMod(ModContentPack content) : base(content)
+    {
+        Log.Message("Hello world from Grey Goo");
+
+        // initialize settings
+        settings = GetSettings<Settings>();
+#if DEBUG
+        Harmony.DEBUG = true;
+#endif
+        Harmony harmony = new Harmony("keyz182.rimworld.Grey_Goo.main");	
+        harmony.PatchAll();
+    }
+
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        base.DoSettingsWindowContents(inRect);
+        settings.DoWindowContents(inRect);
+    }
+
+    public override string SettingsCategory()
+    {
+        return "Grey Goo_SettingsCategory".Translate();
+    }
+}
